4f3b86b4fb988588edc9102659da4a582214f47d
ananiasC <ananiascaetano556@gmail.com>
2022-09-21 19:08:26 -0300

    Código base para desenvolvimento de aplicação

---
+++ model/User.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn')
+
+
+const User = db.define('User', {
+  nome: {
+    type: DataTypes.STRING,
+    required: true
+  },
+  idade: {
+    type: DataTypes.INTEGER,
+    required: true
+  },
+
+})
+
+module.exports = User
